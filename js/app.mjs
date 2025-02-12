ab114ecfd788f24c7374d4acfcff0ed6e0024e06
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2025-02-11 23:23:10 -0700

    keepingitmodern

---
+++ js/app.mjs
@@ -0,0 +1,5 @@
+import { attachEventListeners } from "./events.mjs";
+
+document.addEventListener("DOMContentLoaded", () => {
+    attachEventListeners();
+});
---
+++ js/events.mjs
@@ -0,0 +1,5 @@
+export function attachEventListeners() {
+    document.getElementById("plan-trip").addEventListener("click", () => {
+        console.log("Plan Trip button clicked!");
+    });
+}
